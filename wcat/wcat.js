904288c8e658c6942a751c85560889a6dbfaa063
akashchavan0712 <akashbchavan17@gmail.com>
2022-05-28 18:51:36 +0530

    recent Changes

--- wcat/wcat.js
+++ wcat/wcat.js
@@ -1,27 +1,36 @@
 let fs = require("fs");
-
 // node wcat.js filepath => displays the contents of a file in terminal
-
 let input = process.argv.slice(2); 
-
 //slice actually slices the data and skips the input;
 //The Path of NodeJS and The File Path is not printed;
-
 let fileArr = [];
-
-for(let i = 0 ; i < input.length ; i++)
-{
-    fileArr.push(input[i]);
-}
-
+// for(let i = 0 ; i < input.length ; i++)
+// {
+//     fileArr.push(input[i]);
+// }
 // console.log("Files To Be Added Are : "+fileArr);
+// Now Checking whether the files exist or not inside
 
-// Now Checking whether the files exist or not inside 
+let optionsArr = [];
+for(let i=0; i<input.length; i++)
+{
+    let firstChar = input[i].charAt(0);
+    // console.log(firstChar);
+    if(firstChar == '-')
+    {
+        optionsArr.push(input[i]);
+    }
+    else
+    {
+        fileArr.push(input[i]);
+    }
+}
 
 for(let i=0; i<fileArr.length; i++)
 {
     let doesExist = fs.existsSync(fileArr[i]);
-    if (!doesExist){
+    if (!doesExist)
+    {
         console.log("Files Does Not Exist");
     }
 }
@@ -32,8 +41,27 @@ let content = "";
 for(let i = 0; i < fileArr.length; i++)
 {
     let fileContent = fs.readFileSync(fileArr[i]);
-    content = content + fileContent + "\n";
+    content =  content + fileContent + "\r\n" ;
 }
-console.log(content);
+let contentArr = content.split("\r\n");
+console.table(contentArr);
+// Check Whether -s is present or not
 
-// Appending Data To The File
+let isSPresent = optionsArr.includes('-s');
+// Since The Presence Is Confirmed
+if(isSPresent)
+{
+    for (let i = 1; i < contentArr.length; i++) 
+    {
+        if(contentArr[i] == "" && contentArr[i-1] == "")
+        {
+            contentArr[i] = null;
+        }
+        else if(contentArr[i] == "" && contentArr[i-1] == null)
+        {
+            contentArr[i] = null;
+        }
+    }
+}
+
+console.table(contentArr);
